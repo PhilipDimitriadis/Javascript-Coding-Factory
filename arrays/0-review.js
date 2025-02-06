72af8a7ce7827b3277b5bf4f63f9b874bab74f2a
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-02-06 18:47:00 +0200

    updates

--- arrays/0-review.js
+++ arrays/0-review.js
@@ -16,13 +16,15 @@ cities.sort((a, b) => a.localeCompare(b, "en"))
 const citiesSorted = cities.slice().sort((a, b) => a.localeCompare(b, "en"))     // Shallow Copy
 
 
-// Copy Deeo Copy
+// Copy Deep Copy
 const citiesCopy1 = JSON.parse(JSON.stringify(cities))
 
 const citiesCopy2 = structuredClone(cities)
 
 // Shallow Copies -- Spread Operator
 const citiesCopy3 = [...cities]
-let maxVal = Math.max(...[1, 2, 3, 4])
+
+const nums = [1, 2, 3, 4]
+let maxVal = Math.max(...nums)
 let maxIndex = cities.indexOf(maxVal)
 console.log(`Max Value: ${maxVal}, Max Index: ${maxIndex} `)
